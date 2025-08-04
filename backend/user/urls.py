a8d3291ab90976ff038a30c4cf50352dace39b37
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-08-04 23:36:34 +0300

    Api url change

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, MeView
+from .views import RegisterView, LoginView, LogoutView, MeView, RefreshAccessTokenView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
+    path('refresh_token/', RefreshAccessTokenView.as_view()),
     path('me/', MeView.as_view()),
 ]
